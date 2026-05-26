f57acae8e21bdcaca4813822a85c49702d38290f
Mauricio Navarrete <a367785@uach.mx>
2026-05-26 10:07:55 -0600

    Correccion del script main.py

--- main.py
+++ main.py
@@ -15,26 +15,41 @@ for year in range(2015, 2025):
     df = pd.read_csv(
         file,
         encoding="latin1",
-        low_memory=False
+        low_memory=False,
+        dtype=str
     )
 
+    # Limpiar nombres de columnas
+    df.columns = df.columns.str.strip()
+
+    # Quitar BOM si existe
+    df.columns = df.columns.str.replace("ï»¿", "", regex=False)
+
+    # Limpiar espacios en datos
+    df["ID_ENTIDAD"] = df["ID_ENTIDAD"].str.strip()
+
+    # Filtrar Chihuahua
     df = df[
-        df["ID_ENTIDAD"].astype(str).str.zfill(2) == "08"
+        df["ID_ENTIDAD"].str.zfill(2) == "08"
     ]
 
     dfs.append(df)
 
+# Unir todo
 final_df = pd.concat(dfs, ignore_index=True)
 
+# Crear dataset Ray
 ds = rd.from_pandas(final_df)
 
 print(ds.take(5))
 
 print("Total registros:", ds.count())
 
+# Guardar CSV correcto
 final_df.to_csv(
     "chihuahua_2015_2024.csv",
-    index=False
+    index=False,
+    encoding="utf-8-sig"
 )
 
 print("Archivo generado correctamente")
