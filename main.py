a9be65cc033c7adf30b54286cd121fa0e9898b2f
Mauricio Navarrete <a367785@uach.mx>
2026-05-28 07:08:21 -0600

    Ultima correccion

--- main.py
+++ main.py
@@ -19,33 +19,26 @@ for year in range(2015, 2025):
         dtype=str
     )
 
-    # Limpiar nombres de columnas
     df.columns = df.columns.str.strip()
 
-    # Quitar BOM si existe
     df.columns = df.columns.str.replace("ï»¿", "", regex=False)
 
-    # Limpiar espacios en datos
     df["ID_ENTIDAD"] = df["ID_ENTIDAD"].str.strip()
 
-    # Filtrar Chihuahua
     df = df[
         df["ID_ENTIDAD"].str.zfill(2) == "08"
     ]
 
     dfs.append(df)
 
-# Unir todo
 final_df = pd.concat(dfs, ignore_index=True)
 
-# Crear dataset Ray
 ds = rd.from_pandas(final_df)
 
 print(ds.take(5))
 
 print("Total registros:", ds.count())
 
-# Guardar CSV correcto
 final_df.to_csv(
     "chihuahua_2015_2024.csv",
     index=False,
